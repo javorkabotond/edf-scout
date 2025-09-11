bbd2afc038b789e0c8bec6c771c3e37a01456439
Botond Javorka <botond.javorka@sap.com>
2025-09-11 18:20:15 +0200

    init zeto hw project

---
+++ src/main/java/hu/javorkabotond/edfscout/EdfScoutApplication.java
@@ -0,0 +1,13 @@
+package hu.javorkabotond.edfscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdfScoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdfScoutApplication.class, args);
+    }
+
+}

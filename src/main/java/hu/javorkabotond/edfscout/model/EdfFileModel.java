335e04898b50549703ea4ec918c2f4ce38124202
Botond Javorka <botond.javorka@sap.com>
2025-09-21 13:22:47 +0200

    feat(model): create edf file model

--- src/main/java/hu/javorkabotond/edfscout/model/EdfFileModel.java
+++ src/main/java/hu/javorkabotond/edfscout/model/EdfFileModel.java
@@ -1,13 +1,17 @@
 package hu.javorkabotond.edfscout.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EdfFileModel {
     private String fileName;
     private boolean valid;
